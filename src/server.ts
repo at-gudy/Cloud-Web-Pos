a3d2164d5e91520a189912da760d4aac95591e10
winetree94 <winetree94@outlook.com>
2019-06-30 13:41:17 +0900

    20190630

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import app from './app';
 import logger from './util/logger';
 
+
 const server = app.listen(app.get('port'), () => {
   logger.debug('Server Environment : ' + process.env.NODE_ENV);
   logger.debug('Server Port : ' + process.env.PORT);
